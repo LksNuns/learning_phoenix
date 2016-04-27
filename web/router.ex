b0e01acd6580134fb57142dc70bb3c11eb62c7b8
Lucas Arvani <lksnunss@gmail.com>
2016-04-27 19:51:49 -0300

    new user page created

--- web/router.ex
+++ web/router.ex
@@ -16,8 +16,7 @@ defmodule Rumbl.Router do
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
 
-    get "/users", UserController, :index
-    get "/user/:id", UserController, :show
+    resources "/users", UserController, only: [:index, :show, :new, :create]
     get "/", PageController, :index
   end
 
