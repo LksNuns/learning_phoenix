b46ce150bace6b55e87b702be3a168dc44e0c85b
Lucas Arvani <lksnunss@gmail.com>
2016-05-30 23:59:08 -0300

    create categories and relationship with video

---
+++ priv/repo/migrations/20160531025632_add_category_id_to_video.exs
@@ -0,0 +1,8 @@
+defmodule Rumbl.Repo.Migrations.AddCategoryIdToVideo do
+  use Ecto.Migration
+  def change do
+    alter table(:videos) do
+      add :category_id, references(:categories)
+    end
+  end
+end
--- web/models/category.ex
+++ web/models/category.ex
@@ -1,18 +1,14 @@
-defmodule Rumbl.Video do
+defmodule Rumbl.Category do
   use Rumbl.Web, :model
 
-  schema "videos" do
-    field :url, :string
-    field :title, :string
-    field :description, :string
-    belongs_to :user, Rumbl.User
-    belongs_to :categories, Rumbl.Category
+  schema "categories" do
+    field :name, :string
 
     timestamps
   end
 
-  @required_fields ~w(url title description)
-  @optional_fields ~w(category_id)
+  @required_fields ~w(name)
+  @optional_fields ~w()
 
   @doc """
   Creates a changeset based on the `model` and `params`.
